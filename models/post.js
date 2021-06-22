d81e4527aa4020e30702868aface2a93efa395b4
Christian Bolus <christianjbolus@gmail.com>
2021-06-22 15:50:46 -0400

    Create post schema

---
+++ models/post.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const Post = new Schema(
+    {
+        author: {
+            name: {
+                type: String,
+                maxLength: 24,
+                required: true
+            },
+            email: {
+                type: String,
+                maxLength: 80,
+                required: true
+            },
+            bio: {
+                type: String,
+                maxLength: 240,
+            }
+        },
+        content: {
+            title: {
+                type: String,
+                maxLength: 40,
+                required: true
+            },
+            body: {
+                type: String,
+                maxLength: 5000,
+                required: true
+            },
+            isPublished: {
+                type: Boolean,
+                default: true
+            },
+            publishDate: {
+                type: Date
+            }
+        }
+    },
+    {timestamps: true}
+)
+
+export default mongoose.model('posts', Post)
