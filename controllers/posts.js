75645b7051a7152d66cb8190d6164011f8b52a20
Christian Bolus <christianjbolus@gmail.com>
2021-06-23 11:29:37 -0400

    Add create functionality

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,2 +1,34 @@
-import Post from '../models/post.js'
+import Post from '../models/post.js';
 
+export const getPosts = async (req, res) => {
+  try {
+    const posts = await Post.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
+
+export const getOnePost = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await Post.findById(id);
+    if (post) {
+      res.status(200).res.json(post);
+    } else {
+      throw new Error();
+    }
+  } catch (error) {
+    res.status(404).send('Post not found.');
+  }
+};
+
+export const createPost = async (req, res) => {
+  try {
+    const post = new Post(req.body);
+    await post.save();
+    res.status(201).json(post)
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+};
---
+++ routes/posts.js
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+import * as controllers from '../controllers/posts.js'
+
+const router = Router()
+
+router.get('/posts', controllers.getPosts)
+router.get('/posts/:id', controllers.getOnePost)
+router.post('/post', controllers.createPost)
+
+export default router
