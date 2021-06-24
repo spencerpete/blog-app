437eee4eeb1fd6427597fc97b6c480493784220c
Spencer Peterson <spencerpeterson@MacBook-Pro-4.lan>
2021-06-24 16:51:09 -0400

    reverted app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,6 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import PostDetail from './screens/PostDetail/PostDetail';
 function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/posts/:id" component={PostDetail} />
-      </Switch>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
